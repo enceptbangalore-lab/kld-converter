ebcc3dfe2fa0fd9565bc7d902c45b420b15a6ac0
enceptbangalore-lab <encept.bangalore@gmail.com>
2025-11-13 14:53:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ def make_svg(data):
 
     # BOTTOM-RIGHT
     out.append(f'<line x1="{W + crop_off}" y1="0" x2="{W + crop_off + crop_len}" y2="0" class="dieline"/>')      # horizontal → right
-    out.append(f'<line x1="{W}" y1="0" x2="{W}" y2="{-crop_off - crop_len}" class="dieline"/>')                  # vertical ↓ down
+    out.append(f'<line x1="{W}" y1=crop_off x2="{W}" y2="{-crop_off - crop_len}" class="dieline"/>')                  # vertical ↓ down
 
     out.append('</g>')
     out.append('</svg>')
