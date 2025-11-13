225cc7c64bfadd88b155e958a994ac80aad588d6
enceptbangalore-lab <encept.bangalore@gmail.com>
2025-11-13 13:34:01 +0530

    app.py

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ def make_svg(data):
     crop_len = 5.0
 
     out = []
-    out.append(f'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {W} {H}">')
+out.append(f'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {W} {H}">')
 out.append('<defs><style><![CDATA[')
 out.append(f'.dieline{{stroke:{dieline};stroke-width:{stroke_pt}pt;fill:none;}}')
 out.append(f'.text{{font-family:Arial; font-size:8pt; fill:{dieline};}}')
