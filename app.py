5ac3912c7700ef86d292dd309b9168a1dc6223e0
enceptbangalore-lab <encept.bangalore@gmail.com>
2025-11-20 21:37:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -285,7 +285,7 @@ def make_svg(data, line_spacing_mm=5.0):
     canvas_H = H + extra
 
     dieline = "#92278f"
-    stroke_pt = 0.356
+    stroke_pt = 0.190
     font_mm = 0.8
     tick_short = 5
     top_shift_up = 5
